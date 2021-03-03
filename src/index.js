7a5f926ae0c1ca7cceabfff2f9476bdb8f2f0f09
ckachok <mefelion89@gmail.com>
2021-03-03 03:08:25 +0300

    refactor: change code formatting

--- src/index.js
+++ src/index.js
@@ -16,13 +16,13 @@ module.exports = function toReadable (number) {
     }         
     
     if (number < 1000) {          // for numbers from 100 to 999
-        return units[Math.floor(number / 100)] + ' hundred' + (number % 100 === 0 ? '' : ' ' + toReadable(number % 100));
+        return toReadable[Math.floor(number / 100)] + ' hundred' + (number % 100 === 0 ? '' : ' ' + toReadable(number % 100));
     } 
 
     if (number < 1000000) {       // for numbers from 1.000 to 999.999
-        return toReadable(Math.floor(number / 1000)) + " thousand" + (number % 1000 === 0 ? '' : ' ' + toReadable(number % 1000));
+        return toReadable(Math.floor(number / 1000)) + ' thousand' + (number % 1000 === 0 ? '' : ' ' + toReadable(number % 1000));
     } else {                      // for numbers from 1.000.000 to 999.999.999
-        return toReadable(Math.floor(number / 1000000)) + " million" + (number % 1000000 === 0 ? '' : ' ' + toReadable(number % 1000000));
+        return toReadable(Math.floor(number / 1000000)) + ' million' + (number % 1000000 === 0 ? '' : ' ' + toReadable(number % 1000000));
     }
 };
 
